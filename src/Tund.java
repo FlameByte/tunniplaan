6e186cbc2631e940641b863dc3867412a1890f34
Henri Lindret <henrilindret@gmail.com>
2019-12-17 13:58:55 +0200

    tunniplaan lõplik

---
+++ src/Tund.java
@@ -0,0 +1,25 @@
+public class Tund {
+    public String tund;
+    public String algus;
+    public String lopp;
+    public String aine;
+    public String grupp;
+    public String opetaja;
+    public String ruum;
+
+    @Override
+    public String toString() {
+        return  "Tund: " + tund +
+        return  "\nTund: " + tund +
+                "\nAlgus: " + algus +
+                "\nLopp: " + lopp +
+                "\nAine: " + aine +
+                "\nGrupp: " + grupp +
+                "\nOpetaja: " + opetaja +
+                "\nRuum: " + ruum
+                "\nRuum: " + ruum +
+                "\n=======================" +
+                "\n"
+                ;
+    }
+} 
